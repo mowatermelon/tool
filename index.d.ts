715e841a3ba333598d069000bc9925ab09163b9a
eva <neefoxmo@gmail.com>
2021-03-07 15:18:09 +0000

    ci: github action build

--- index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as BaseTool } from './libs/BaseTool';
-export { default as Check } from './libs/Check';
-export { default as Distinct } from './libs/Distinct';
-export { default as Translation } from './libs/Translation';
--- libs/utils/ErrorBoundary/ErrorBoundaryFallback.d.ts
+++
@@ -1,6 +0,0 @@
-interface IProps {
-    componentStack: string;
-    error: Error;
-}
-declare const ErrorBoundaryFallback: ({ componentStack, error }: IProps) => JSX.Element;
-export default ErrorBoundaryFallback;
